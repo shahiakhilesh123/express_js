516a2ee77db7910cd7a646fe24b8e60c31ef22e2
shahiakhilesh123 <89912436+shahiakhilesh123@users.noreply.github.com>
2023-11-05 09:15:20 +0530

    upload done

--- index.js
+++ index.js
@@ -1,34 +1,22 @@
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/node_database');
-const userSchema = new mongoose.Schema({
-        name:String
-});
-mongoose.set("strictQuery", false);
-const savedata = async () => {
-    let UsersModel = mongoose.model('users', userSchema);
-    let data = new UsersModel({name:"akhilesh 2", role_no: "20"});
-    let result = await data.save().catch(error => { throw error});
-    console.log(result);
-}
-const updateData =  async () => {
-    let UsersModel = mongoose.model('users', userSchema);
-    let result = await UsersModel.updateOne(
-        {name: "akhilesh 2"},
-        {
-            $set: {name: "akhilesh 3"}
-        }
-    ).catch(error => { throw error});
-    console.log(result);
-}
-const deleteData = async () => {
-    let UsersModel = mongoose.model('users', userSchema);
-    let result = await UsersModel.deleteOne({name:"akhilesh 3"});
-    console.log(result);
-}
+const express = require('express');
+const multer =  require('multer');
+const app = express();
 
-const showData = async () => {
-    let UsersModel = mongoose.model('users', userSchema);
-    let result = await UsersModel.find();
-    console.log(result);
-}
-showData();
+const upload = multer({
+    storage:multer.diskStorage({
+        destination:function(req, file, cb)
+        {
+            cb(null, "uploads")
+        },
+        filename:function(req, file, cb)
+        {
+            cb(null, file.fieldname+"_"+Date.now()+".jpg")
+        }
+    })
+}).single('user_file');
+
+app.post('/upload', upload, (req, resp)=>{
+    resp.send('File Uploaded');
+})
+
+app.listen(5000);
