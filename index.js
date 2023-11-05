f448a208ff94e5ae7a96a0afcd6d699b4b77c99e
shahiakhilesh123 <89912436+shahiakhilesh123@users.noreply.github.com>
2023-11-05 09:56:26 +0530

    upload done

--- index.js
+++ index.js
@@ -1,22 +1,8 @@
-const express = require('express');
-const multer =  require('multer');
-const app = express();
-
-const upload = multer({
-    storage:multer.diskStorage({
-        destination:function(req, file, cb)
-        {
-            cb(null, "uploads")
-        },
-        filename:function(req, file, cb)
-        {
-            cb(null, file.fieldname+"_"+Date.now()+".jpg")
-        }
-    })
-}).single('user_file');
-
-app.post('/upload', upload, (req, resp)=>{
-    resp.send('File Uploaded');
-})
-
-app.listen(5000);
+const os = require('os');
+//console.log(os.arch());
+// console.log(os.freemem()/(1024*1024*1024));
+// console.log(os.totalmem()/(1024*1024*1024));
+// console.log(os.hostname());
+//console.log(os.homedir());
+// console.log(os.platform());
+console.log(os.userInfo());
