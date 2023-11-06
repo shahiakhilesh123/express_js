27581473bb1974c90ad5f9d8fc5d21fc532e720e
shahiakhilesh123 <89912436+shahiakhilesh123@users.noreply.github.com>
2023-11-06 13:54:06 +0530

    my sql connection

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-var x = 1;
-x = parseInt(x);
-console.log(x++);
+const mysql = require('mysql');
+const con = mysql.createConnection({
+    host: "localhost",
+    user:"root",
+    password:"root",
+    database: "medical"
+})
+con.connect((err)=>{
+    if(err) {
+        console.log(err);
+    } else {
+        console.log('Connected');
+    }
+});
+con.query('select * from m_company', (err, result)=>{
+    console.log(result);
+});
