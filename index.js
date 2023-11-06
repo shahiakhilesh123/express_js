e67c4869c68a8e4eec88e21bf55db5950319a2e3
shahiakhilesh123 <89912436+shahiakhilesh123@users.noreply.github.com>
2023-11-06 12:53:56 +0530

    REPL example

--- index.js
+++ index.js
@@ -1,18 +1,3 @@
-const express = require('express');
-const EventEmitter = require('events');
-
-const app = express();
-const event = new EventEmitter();
-let count = 0;
-
-event.on("counterApi", ()=>{
-    count++;
-    console.log('event called '+count);
-})
-
-app.get('/', ()=>{
-    console.log('Event Api')
-    event.emit('counterApi')
-})
-
-app.listen(5000);
+var x = 1;
+x = parseInt(x);
+console.log(x++);
